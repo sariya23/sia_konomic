09c87979f643d2dc124890ca3367cd8d231ecde8
sariya <dayzwaitik123@yandex.ru>
2024-02-28 23:38:17 +0300

    Add clases with locators

---
+++ tests/registration_page/common/css_locators.py
@@ -0,0 +1,4 @@
+class CSSLocators:
+    SHADOW_HOST = ".remoteComponent"
+    USER_NAME_FIELD = "#input-135"
+    WARNING_ELEMENT_USERNAME = "div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div:nth-child(1) > div > div > div.v-text-field__details > div > div > div > div > div > span"
---
+++ tests/registration_page/common/xpath_locators.py
@@ -0,0 +1,2 @@
+class XPATHLocators:
+    LOGO_ELEMENT = "(//*[@class='router-link-active'])[1]"
