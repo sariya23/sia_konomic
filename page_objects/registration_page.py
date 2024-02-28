0c26a96adc1bf61890a1036570adc23eee2113b8
sariya <dayzwaitik123@yandex.ru>
2024-02-28 19:10:34 +0300

    delete imports

--- page_objects/registration_page.py
+++ page_objects/registration_page.py
@@ -1,10 +1,7 @@
 from pageo import BasePage
-from pageo.locators import XPATHLocator
 
 from tests.common.urls import URLs
 
 class RegistrationPage(BasePage):
     base_url = URLs.BASE_URL
     url_suffix = URLs.REGISTRATION_SUFFIX
-
-    
