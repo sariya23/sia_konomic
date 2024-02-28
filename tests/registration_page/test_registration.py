9a671bf479230516115d39fb1b75beab53248188
sariya <dayzwaitik123@yandex.ru>
2024-02-28 19:11:02 +0300

    Work shadow root

--- tests/registration_page/test_registration.py
+++ tests/registration_page/test_registration.py
@@ -1,9 +1,15 @@
 from page_objects.registration_page import RegistrationPage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-import time
 
 def test_a(driver):
-    page = RegistrationPage(driver)
-    time.sleep(2)
-
+    _ = RegistrationPage(driver, window_size=(640, 400))
+    wait = WebDriverWait(driver, 10)
+    _ = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-header"]/div[4]/div/div[2]/a/span')))
+    shadow_host = driver.find_element(By.CSS_SELECTOR, ".remoteComponent")
+    shadow_root = shadow_host.shadow_root
+    el = shadow_root.find_element(By.CSS_SELECTOR, '#input-135')
+    print(el)
     assert False
