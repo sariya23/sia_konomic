614e2f066db8345bfcc45ff4f1c2c8372bbc1015
sariya <dayzwaitik123@yandex.ru>
2024-02-28 23:39:53 +0300

    Use locator classes

--- tests/registration_page/test_registration.py
+++ tests/registration_page/test_registration.py
@@ -9,13 +9,18 @@ import pytest
 
 from common.usernames import incorrect_usernames 
 from common.text_elements import TextElements
+from common.css_locators import CSSLocators
+from common.xpath_locators import XPATHLocators
 
 
+@pytest.mark.tags('mobile', 'tablet', 'desktop')
 @pytest.mark.parametrize("username", incorrect_usernames)
 def test_username_field(driver: webdriver, username: str):
     """
     Проверяем, что при вводе username'а неверного формата появляется
     предупреждающие сообщение.
+
+    При вводе 'ничего', сообщение другое
 
     Как проверяем:
     1. Вводим неверное значение имени пользователя;
@@ -23,16 +28,14 @@ def test_username_field(driver: webdriver, username: str):
     """
     _ = RegistrationPage(driver, window_size=(640, 500))
     wait = WebDriverWait(driver, 20)
-    _ = wait.until(EC.element_to_be_clickable((By.XPATH, "(//*[@class='router-link-active'])[1]")))
-    shadow_host = driver.find_element(By.CSS_SELECTOR, ".remoteComponent")
+    wait.until(EC.element_to_be_clickable((By.XPATH, XPATHLocators.LOGO_ELEMENT)))
+    shadow_host = driver.find_element(By.CSS_SELECTOR, CSSLocators.SHADOW_HOST)
     shadow_root = shadow_host.shadow_root
-    username_field = shadow_root.find_element(By.CSS_SELECTOR, "#input-135")
+    username_field = shadow_root.find_element(By.CSS_SELECTOR, CSSLocators.USER_NAME_FIELD)
     username_field.send_keys(username)
     action = ActionChains(driver)
-    action.send_keys(Keys.TAB)
-    action.perform()
+    action.send_keys(Keys.TAB).perform()
 
 
-    warning_element = shadow_root.find_element(By.CSS_SELECTOR, "div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div:nth-child(1) > div > div > div.v-text-field__details > div > div > div > div > div > span")
-
+    warning_element = shadow_root.find_element(By.CSS_SELECTOR, CSSLocators.WARNING_ELEMENT_USERNAME)
     assert warning_element.text == TextElements.WARNING_INVALID_USERNAME
