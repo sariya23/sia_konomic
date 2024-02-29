e113003b56c173b449b239ef83c87871995e8659
sariya <dayzwaitik123@yandex.ru>
2024-02-29 12:25:02 +0300

    Use seed for run tests in miltupreccess

--- tests/registration_page/common/usernames.py
+++ tests/registration_page/common/usernames.py
@@ -1,6 +1,7 @@
 from string import punctuation
 import random
 
+random.seed(5)
 cyrilic = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
 
 incorrect_usernames = (
@@ -14,3 +15,5 @@ incorrect_usernames = (
     *[f"abo{symbol}ba" for symbol in punctuation if symbol != '_'],
     *["".join(random.sample(cyrilic, word_len)) for word_len in range(6, 33)]
 )
+
+print(incorrect_usernames)
