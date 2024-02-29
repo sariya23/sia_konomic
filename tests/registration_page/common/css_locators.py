e79b8c961a490bc0239b6950b25e40fa4afda584
sariya <dayzwaitik123@yandex.ru>
2024-02-29 12:59:41 +0300

    Add locators

--- tests/registration_page/common/css_locators.py
+++ tests/registration_page/common/css_locators.py
@@ -2,3 +2,6 @@ class CSSLocators:
     SHADOW_HOST = ".remoteComponent"
     USER_NAME_FIELD = "#input-135"
     WARNING_ELEMENT_USERNAME = "div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div:nth-child(1) > div > div > div.v-text-field__details > div > div > div > div > div > span"
+    EMAIL_FIELD = "#username"
+    WARNING_ELEMENT_EMAIL = "div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div:nth-child(2) > div > div > div.v-text-field__details > div > div > div > div > div > span"
+    
--- tests/registration_page/common/text_elements.py
+++ tests/registration_page/common/text_elements.py
@@ -1,2 +1,3 @@
 class TextElements:
     WARNING_INVALID_USERNAME = "Допустимые символы (от 6 до 32): a-z, 0-9, _. Имя должно начинаться с буквы"
+    WARNING_INVALID_EMAIL = "Формат e-mail: somebody@somewhere.ru"
