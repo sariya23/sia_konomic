f2b46b58f9d6f0284c24b2d471c348b22f8f6403
sariya <dayzwaitik123@yandex.ru>
2024-02-29 13:16:37 +0300

    Delete email with spaces

--- tests/registration_page/common/emails.py
+++ tests/registration_page/common/emails.py
@@ -2,11 +2,9 @@ incorrect_emails = (
     "aboba@invalid-tld.123",
     "aboba#domain.com",
     "aboba#domain.con",
-    "spaced aboba@domain.info",
     "double..dots@email.org",
     "@.com",
     "aboba@domain with space.com"
     "aboba@domain..com",
     'a"b(c)d,e:f;g<h>i[j\k]l@example.com',
-
 )
